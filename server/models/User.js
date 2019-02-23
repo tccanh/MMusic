30daba2b0f60da7cc79f792d4782b205363bf270
Canh <safetycanh@gmail.com>
2019-02-23 13:16:02 +0700

    init

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+/* eslint-disable comma-dangle */
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  role: {
+    default: "USER",
+    enum: ["USER", "ADMIN", "MOD", "GUESS"],
+    required: true
+  }
+});
+const User = mongoose.model("users", UserSchema);
+module.exports = User;
